fab63cb8110ba4b3a0978f0b3670ae257318cfe8
BBQueiroz <bruninhoqueiroz@gmail.com>
2024-03-30 04:59:19 -0300

    Update CorsConfig.java

--- src/main/java/com/dovisen/dblog/infra/CorsConfig.java
+++ src/main/java/com/dovisen/dblog/infra/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:4200"));
+        config.setAllowedOrigins(List.of("https://dblog-angular-swart.vercel.app"));
         config.setAllowCredentials(true);
         config.addAllowedHeader("*");
         config.addAllowedMethod("GET");
