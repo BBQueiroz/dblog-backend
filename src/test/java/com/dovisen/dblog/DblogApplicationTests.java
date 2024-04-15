cf5b688fc1ae9bc8f2194e5b5423adece7c73233
BBQueiroz <bruninhoqueiroz@gmail.com>
2024-04-15 20:39:33 -0300

    Update DblogApplicationTests.java

--- src/test/java/com/dovisen/dblog/DblogApplicationTests.java
+++ src/test/java/com/dovisen/dblog/DblogApplicationTests.java
@@ -3,7 +3,7 @@ package com.dovisen.dblog;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = DblogApplication.class)
 class DblogApplicationTests {
 
 	@Test
