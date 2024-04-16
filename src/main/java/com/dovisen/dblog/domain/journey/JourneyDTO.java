63d4697a45c78f5fe17bcf5b16ddd18a567289ed
BBQueiroz <bruninhoqueiroz@gmail.com>
2024-04-15 21:00:50 -0300

    Journey removed to mantain scope of the application
    Journey is no longer in the scope of dblog application and will be moved to it's own application

--- src/main/java/com/dovisen/dblog/domain/journey/JourneyDTO.java
+++
@@ -1,9 +0,0 @@
-package com.dovisen.dblog.domain.journey;
-
-import com.dovisen.dblog.domain.user.User;
-
-import java.util.List;
-import java.util.UUID;
-
-public record JourneyDTO(UUID id, JourneyNode root, String text, User user, List<JourneyNode> children, JourneyNode parent) {
-}
